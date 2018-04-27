9c52d03569523da4031da5eafce55216d52426e5
Fabián Alexis Bravo Abarca <fabian.bravo.abarca@gmail.com>
2018-04-27 10:37:26 -0300

    Primer commit

---
+++ app.js
@@ -0,0 +1,39 @@
+window.onload = function() {
+    var name = prompt("¿cuál es tu nombre?");
+    document.getElementById("name").innerText = name;
+
+    var wantToPlay = prompt("¿quieres jugar? s/n");
+    if (wantToPlay.toLowerCase() == "s") {
+        var answer1 = prompt("¿han egresado hombres en Laboratoria? s/n");
+        if (answer1.toLowerCase() == "n") {
+            document.getElementById("rightAnswersText").innerHTML =
+                document.getElementById("rightAnswersText").innerHTML +
+                "<div class='answer'>" +
+                "<p>No han egresado hombres</p>"
+            "</div>";
+        } else {
+            document.getElementById("wrongAnswersText").innerHTML =
+                document.getElementById("wrongAnswersText").innerHTML +
+                "<div class='answer'>" +
+                "<p>No han egresado hombres</p>"
+            "</div>";
+        }
+
+        var answer2 = prompt("¿hay laboratoria en concepción? s/n");
+        if (answer2.toLowerCase() == "n") {
+            document.getElementById("rightAnswersText").innerHTML =
+                document.getElementById("rightAnswersText").innerHTML +
+                "<div class='answer'>" +
+                "<p>No hay laboratoria en concepción</p>"
+            "</div>";
+        } else {
+            document.getElementById("wrongAnswersText").innerHTML =
+                document.getElementById("wrongAnswersText").innerHTML +
+                "<div class='answer'>" +
+                "<p>No hay laboratoria en concepción</p>"
+            "</div>";
+        }
+    } else {
+        document.getElementById("warningMessage").innerText = "Bueno Chao";
+    }
+}
