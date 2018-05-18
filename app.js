0308469afa8b5c9787bcaa00d33b1d80ed8d4afb
Mi Nombre <valeriadroguettl@gmail.com>
2018-05-18 17:28:38 -0400

    No funcionó js (hasta tienes que escribir tu nombre para jugar número 1), eliminar todo.

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
 window.onload = function() {
-    
+    var name;
+    document.getElementById("enterName").onclick = function() {
+        name = document.getElementById("casillaNombre").value;
+        if(name == ""){
+            document.getElementById("warningMessage").innerText = "Tienes que escribir tu nombre para jugar";
+
+        }
+    }
 }
